f55c695f2c4db85ae3346d52105580190414ee95
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-02-25 22:45:04 +0300

    refactor: delete custom basic UI React components

--- src/Components/UI/Article/Article.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Article.module.scss';
-import * as React from 'react';
-
-type ArticleProps = React.HTMLAttributes<HTMLElement>;
-
-export default function Article(props: ArticleProps) {
-  return <article className={styles.article} {...props}></article>;
-}
