fad622c68eebe752b169c501b3ff01be169ba87a
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-03-02 12:08:34 +0100

    feat: add interfaces for a quiz data

--- src/features/WidgetQuiz/widgetQuizTypes.ts
+++ src/features/WidgetQuiz/widgetQuizTypes.ts
@@ -2,4 +2,19 @@ interface QuizType {
   type: 'html' | 'css' | 'javascript' | 'typescript' | 'github';
 }
 
-export type { QuizType };
+type UserAnswer = number | null;
+
+interface QuizQuestion {
+  isText: boolean;
+  question: string;
+  code?: string;
+  answers: [number, string][];
+}
+
+interface QuizProps {
+  quizName: string;
+  questions: QuizQuestion[];
+  rightAnswers: UserAnswer[];
+}
+
+export type { QuizType, UserAnswer, QuizQuestion, QuizProps };
