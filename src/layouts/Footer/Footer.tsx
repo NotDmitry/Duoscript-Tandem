54506065cb7dfa4c6deecd7c29af79bd8d541e64
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2026-03-02 11:54:45 +0100

    feat: add Footer component

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import { Box, Container } from '@mui/material';
+import { FooterLink } from './FooterLink';
+
+export function Footer() {
+  return (
+    <Box component="footer" sx={{ p: 2 }}>
+      <Container sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+        <FooterLink
+          linkName="RS SCHOOL"
+          linkHref="https://rs.school/"
+        ></FooterLink>
+        <FooterLink
+          linkName="Dima"
+          linkHref="https://github.com/NotDmitry"
+        ></FooterLink>
+        <FooterLink
+          linkName="Sveta"
+          linkHref="https://github.com/SwetlanaAng"
+        ></FooterLink>
+        <FooterLink
+          linkName="Alena"
+          linkHref="https://github.com/anelka777"
+        ></FooterLink>
+        <FooterLink
+          linkName="Aleksei"
+          linkHref="https://github.com/ngInit"
+        ></FooterLink>
+      </Container>
+    </Box>
+  );
+}
