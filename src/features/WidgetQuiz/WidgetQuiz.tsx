dbf0b50539efaa08623c5379bac1a5866ad10a4c
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-03-02 11:53:28 +0100

    feat: add base Widget Quiz

---
+++ src/features/WidgetQuiz/WidgetQuiz.tsx
@@ -0,0 +1,33 @@
+import { FormLabel, Container, CircularProgress } from '@mui/material';
+import type { QuizType } from '@/features/WidgetQuiz/widgetQuizTypes.ts';
+
+function WidgetQuiz(quizType: QuizType) {
+  if (quizType.type !== 'css') {
+    return (
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '-webkit-fill-available',
+        }}
+      >
+        <CircularProgress size={40} color="primary" />
+        <FormLabel>Loading quiz...</FormLabel>
+      </Container>
+    );
+  }
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '-webkit-fill-available',
+      }}
+    ></Container>
+  );
+}
+
+export default WidgetQuiz;
