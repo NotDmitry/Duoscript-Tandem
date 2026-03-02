cc5c08e9a20d916e5e3bedfb34f274196b465ae2
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-03-02 13:38:48 +0100

    feat: add an interactive section to the widget

--- src/features/WidgetQuiz/WidgetQuiz.tsx
+++ src/features/WidgetQuiz/WidgetQuiz.tsx
@@ -1,7 +1,18 @@
-import { FormLabel, Container, CircularProgress } from '@mui/material';
-import type { QuizType } from '@/features/WidgetQuiz/WidgetQuiz.types.ts';
+import {
+  Box,
+  Radio,
+  FormLabel,
+  Container,
+  RadioGroup,
+  CircularProgress,
+  FormControlLabel,
+  Button,
+} from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
+import type { QuizType } from './WidgetQuiz.types.ts';
 
 function WidgetQuiz(quizType: QuizType) {
+  console.log(quizType);
   if (quizType.type !== 'css') {
     return (
       <Container
@@ -26,7 +37,84 @@ function WidgetQuiz(quizType: QuizType) {
         flexDirection: 'column',
         height: '-webkit-fill-available',
       }}
-    ></Container>
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          rowGap: 1,
+          paddingBottom: 1,
+        }}
+      >
+        <FormLabel sx={{ typography: 'h4' }}>CSS Quiz</FormLabel>
+        <FormLabel sx={{ typography: 'subtitle1' }}>Question 1 of 45</FormLabel>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '-webkit-fill-available',
+        }}
+      >
+        <Box>
+          <FormLabel>What will be if...</FormLabel>
+          <Box
+            component="pre"
+            sx={{
+              bgcolor: blue['50'],
+              padding: 2,
+              borderRadius: '16px',
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              borderColor: grey['500'],
+            }}
+          >
+            <code
+              style={{ textWrap: 'wrap' }}
+            >{`.class {blah-blah-blah}`}</code>
+          </Box>
+        </Box>
+        <Box>
+          <RadioGroup
+            aria-labelledby="radio-buttons-answers-group-label"
+            name="radio-buttons-answers"
+            value={'1'}
+            sx={{
+              marginLeft: 2,
+            }}
+          >
+            {[
+              [1, 'one'],
+              [2, 'two'],
+            ].map(([index, label]) => (
+              <FormControlLabel
+                key={index}
+                value={String(index)}
+                control={<Radio size="small" sx={{ padding: '6px' }} />}
+                label={label}
+              />
+            ))}
+          </RadioGroup>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          columnGap: 10,
+        }}
+      >
+        <Button variant="contained" color="error">
+          Skip
+        </Button>
+        <Button variant="contained" color="success">
+          Next
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
