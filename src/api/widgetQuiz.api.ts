cd16569e9df7dbc3e9a7fc90b4617c5eee13b8e6
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-03-06 13:19:47 +0100

    feat: add conditionals for a repository pattern

--- src/api/widgetQuiz.api.ts
+++ src/api/widgetQuiz.api.ts
@@ -10,10 +10,23 @@ import type {
   QuizProps,
 } from '@/features/WidgetQuiz/WidgetQuiz.types.ts';
 
+const isRealData = import.meta.env.MOCK_DATA === 'true';
+
 const delay = (time: number) =>
   new Promise((emptyHandler) => setTimeout(emptyHandler, time));
 
 async function getQuizData(quizType: QuizType): Promise<QuizProps> {
+  if (isRealData) {
+    return await fetch('http://localhost:8080').then(
+      (response): Promise<QuizProps> => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.resolve(HTMLQuizParams);
+      }
+    );
+  }
+
   await delay(500);
   switch (quizType.type) {
     case 'html':
