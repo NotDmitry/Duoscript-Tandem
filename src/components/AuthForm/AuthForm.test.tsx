339f8bd5c866f809c9ec31736b2eceb383981de9
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-14 20:39:27 +0300

    refactor: rewrite AuthForm.test using jest-dom matchers

--- src/components/AuthForm/AuthForm.test.tsx
+++ src/components/AuthForm/AuthForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router';
 import type { ReactElement } from 'react';
 import AuthForm from './AuthForm';
@@ -11,24 +11,20 @@ describe('AuthForm', () => {
   it('renders title', () => {
     renderWithRouter(<AuthForm mode="LOGIN" />);
     const text = screen.getByText('WELCOME');
-    expect(text).not.toBeNull();
+    expect(text).toBeInTheDocument();
   });
+
   it('renders inputs - text, password if mode LOGIN', () => {
     renderWithRouter(<AuthForm mode="LOGIN" />);
-    const form = screen.getAllByRole('form');
-
-    const nicknameInput = within(form[0]).getByPlaceholderText('Nickname');
-    const passwordInput = within(form[0]).getByPlaceholderText('Password');
-    expect(nicknameInput).not.toBeNull();
-    expect(passwordInput).not.toBeNull();
+    const nicknameInput = screen.getByPlaceholderText('Nickname');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    expect(nicknameInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
   });
+
   it('renders input - repeat password if mode SIGN UP', () => {
     renderWithRouter(<AuthForm mode="SIGN UP" />);
-    const form = screen.getAllByRole('form');
-
-    const repeatPasswordInput = within(form[0]).getByPlaceholderText(
-      'Repeat Password'
-    );
-    expect(repeatPasswordInput).not.toBeNull();
+    const repeatPasswordInput = screen.getByPlaceholderText('Repeat Password');
+    expect(repeatPasswordInput).toBeInTheDocument();
   });
 });
