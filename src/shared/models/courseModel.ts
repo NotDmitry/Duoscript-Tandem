92bfbc50081a324c59d1f65e2aa634a61ac9b502
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-21 01:00:20 +0300

    refactor: change Course Firestore document model

--- src/shared/models/courseModel.ts
+++ src/shared/models/courseModel.ts
@@ -1,7 +1,13 @@
-export interface Course {
-  id: string;
+import type { Timestamp } from 'firebase/firestore';
+
+export type CourseTag = 'js' | 'ts' | 'css' | 'html' | 'github' | 'algorithms';
+
+export interface CourseDocument {
+  courseId: string;
   title: string;
   description?: string;
   iconUrl?: string;
-  tag: 'js' | 'ts' | 'css' | 'html';
+  tag: CourseTag;
+  lessonIds: string[];
+  createdAt: Timestamp;
 }
