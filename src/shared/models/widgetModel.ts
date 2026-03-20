af72e897077e6b11a83b00ff9378173594b9a6d3
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-21 01:05:51 +0300

    refactor: change Widget Firestore document model

--- src/shared/models/widgetModel.ts
+++ src/shared/models/widgetModel.ts
@@ -1,4 +1,10 @@
-export interface Widget<T> {
-  id: string;
+import type { Timestamp } from 'firebase/firestore';
+
+export type WidgetType = 'quiz';
+
+export interface WidgetDocument<T> {
+  widgetId: string;
+  type: WidgetType;
   config: T;
+  createdAt: Timestamp;
 }
