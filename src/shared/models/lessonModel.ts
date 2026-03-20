810619052b480e1bbc39c8b580ece2aab2a6bd4e
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-21 00:54:22 +0300

    refactor: change Lesson Firestore document model

--- src/shared/models/lessonModel.ts
+++ src/shared/models/lessonModel.ts
@@ -1,6 +1,10 @@
-export interface Lesson {
-  id: string;
+import type { Timestamp } from 'firebase/firestore';
+
+export interface LessonDocument {
+  lessonId: string;
+  courseId: string;
   title: string;
   description?: string;
   widgetId: string;
+  createdAt: Timestamp;
 }
