76a625c633a056e34434007f7f1cea62013ef702
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-03-23 10:52:22 +0100

    feat: add constant data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,19 @@
+const GREETING = 'Hello dear';
+const DEFAULT_USERNAME = 'developer';
+
+const images = [
+  {
+    link: 'src/assets/images/Landing_Example_1.jpg',
+    alt: 'Dashboard example',
+  },
+  {
+    link: 'src/assets/images/Landing_Example_2.jpg',
+    alt: 'Quiz task example',
+  },
+];
+
 function Home() {
+  console.log(GREETING, DEFAULT_USERNAME, images);
   return <div>Home page</div>;
 }
 
