0b5e3b7a86f5b9edac9292dd669fb0b3ff7c76a7
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-26 18:28:00 +0300

    refactor: create model-aligned mocks in separate folder

---
+++ src/mocks/user.mock.ts
@@ -0,0 +1,11 @@
+import type { UserDashboardView } from '@/shared/models/userModel';
+
+export const mockUserDashboard: UserDashboardView = {
+  uid: 'user_1',
+  displayName: 'anelka',
+  progressPercent: 65,
+  progressUpdatedAt: '2026-03-06T00:00:00.000Z',
+  minutesSpent: 45,
+  activitiesCompleted: 2,
+  currentStreak: 3,
+};
