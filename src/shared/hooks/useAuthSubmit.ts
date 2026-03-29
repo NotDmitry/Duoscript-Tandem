1751c1e100785005a1f28eb184c6c82c462902d4
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-29 23:46:03 +0300

    refactor: update useAuthSubmit hook according to new auth types

--- src/shared/hooks/useAuthSubmit.ts
+++ src/shared/hooks/useAuthSubmit.ts
@@ -1,35 +1,43 @@
 import { useCallback, useState } from 'react';
-import type { AuthMode, loginData, registerData } from '../types/auth.types';
+import type {
+  AuthMode,
+  LoginData,
+  RegisterData,
+  UpdateProfileData,
+} from '@/shared/types/auth.types';
 import { useAuth } from './useAuth';
 import { useUI } from './useUI';
+
+const FALLBACK_ERROR_MESSAGES: Record<AuthMode, string> = {
+  LOGIN: 'Email or password is incorrect',
+  'SIGN UP': 'Registration failed',
+  PROFILE: 'Failed to update profile',
+};
 
 export const useAuthSubmit = (mode: AuthMode) => {
   const { showToast } = useUI();
   const { registerFunc, loginFunc, updateProfileFunc } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
+
   const handleAuthSubmit = useCallback(
-    async (formData: loginData | registerData) => {
+    async (formData: LoginData | RegisterData | UpdateProfileData) => {
       setIsLoading(true);
       try {
         if (mode === 'LOGIN') {
-          await loginFunc(formData);
+          await loginFunc(formData as LoginData);
           showToast('You have successfully logged in', 'success');
-          setIsSuccess(true);
         } else if (mode === 'SIGN UP' && 'repeatPassword' in formData) {
           await registerFunc(formData);
           showToast('You have successfully registered', 'success');
-          setIsSuccess(true);
         } else if (mode === 'PROFILE') {
-          await updateProfileFunc(formData);
+          await updateProfileFunc(formData as UpdateProfileData);
           showToast('You have successfully updated profile data', 'success');
-          setIsSuccess(true);
         }
+        setIsSuccess(true);
       } catch (err) {
         const message =
-          err instanceof Error
-            ? err.message
-            : 'Nickname or password is incorrect';
+          err instanceof Error ? err.message : FALLBACK_ERROR_MESSAGES[mode];
         showToast(message, 'error');
         setIsSuccess(false);
       } finally {
