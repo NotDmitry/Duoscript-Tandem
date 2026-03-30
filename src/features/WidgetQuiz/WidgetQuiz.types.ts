51a8214ea166e36eb3f1fee085538542f4d9643f
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-30 18:57:39 +0300

    refactor: update WidgetQuiz with new API call

--- src/features/WidgetQuiz/WidgetQuiz.types.ts
+++ src/features/WidgetQuiz/WidgetQuiz.types.ts
@@ -1,7 +1,3 @@
-interface QuizType {
-  type: 'html' | 'css' | 'javascript' | 'typescript' | 'github';
-}
-
 type UserAnswer = number | null;
 
 interface QuizQuestion {
@@ -11,10 +7,4 @@ interface QuizQuestion {
   answers: [number, string][];
 }
 
-interface QuizProps {
-  quizName: string;
-  questions: QuizQuestion[];
-  rightAnswers: UserAnswer[];
-}
-
-export type { QuizType, UserAnswer, QuizQuestion, QuizProps };
+export type { UserAnswer, QuizQuestion };
