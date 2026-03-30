a5dd11000c7ee8f7ab40c651d7d3349e9b8d2a0d
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-03-30 18:21:25 +0300

    refactor: update widgetQuiz.api according to new mocks

--- src/api/widgetQuiz.api.ts
+++ src/api/widgetQuiz.api.ts
@@ -1,47 +1,12 @@
-import {
-  HTMLQuizParams,
-  CSSQuizParams,
-  JSQuizParams,
-  TSQuizParams,
-  GitHubQuizParams,
-} from '@/features/WidgetQuiz/WidgetQuiz.mock.ts';
-import type {
-  QuizType,
-  QuizProps,
-} from '@/features/WidgetQuiz/WidgetQuiz.types.ts';
+import type { QuizConfig, WidgetView } from '@/shared/models/widgetModel';
+import { quizWidgetMocks } from '@/mocks/widgetQuiz.mock.ts';
 
-const isRealData = import.meta.env.MOCK_DATA === 'true';
+const delay = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
-const delay = (time: number) =>
-  new Promise((emptyHandler) => setTimeout(emptyHandler, time));
-
-async function getQuizData(quizType: QuizType): Promise<QuizProps> {
-  if (isRealData) {
-    return await fetch('http://localhost:8080').then(
-      (response): Promise<QuizProps> => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.resolve(HTMLQuizParams);
-      }
-    );
-  }
-
-  await delay(500);
-  switch (quizType.type) {
-    case 'html':
-      return HTMLQuizParams;
-    case 'css':
-      return CSSQuizParams;
-    case 'javascript':
-      return JSQuizParams;
-    case 'typescript':
-      return TSQuizParams;
-    case 'github':
-      return GitHubQuizParams;
-    default:
-      return CSSQuizParams;
-  }
+export async function getQuizWidget(
+  widgetId: string
+): Promise<WidgetView<QuizConfig>> {
+  await delay(300);
+  return quizWidgetMocks[widgetId];
 }
-
-export default getQuizData;
