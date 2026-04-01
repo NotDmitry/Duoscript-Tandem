659cc270f98db61ea89b262885e7b4715059a6ac
ngInit <aleksey.nikolaev.eng@gmail.com>
2026-04-02 01:03:14 +0200

    refactor: change svg favicon name to logo

--- src/components/Logo/LogoIcon.tsx
+++ src/components/Logo/LogoIcon.tsx
@@ -1,4 +1,4 @@
-import faviconUrl from '@assets/icons/favicon.svg';
+import faviconUrl from '@assets/icons/logo.svg';
 
 export function LogoIcon() {
   return <img src={faviconUrl} alt="Logo" width={24} height={24} />;
