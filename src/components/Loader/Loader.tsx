7e89bce4f2751751ecb9a38d3a0208a943b3bb38
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-04-07 02:03:12 +0300

    feat: add aria-label to the Loader component props

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,12 +2,16 @@ import { Box, CircularProgress } from '@mui/material';
 
 interface LoaderProps {
   p?: number;
+  ariaLabel?: string;
 }
 
-export function Loader({ p = 6 }: LoaderProps) {
+export function Loader({
+  p = 6,
+  ariaLabel = 'Loading in progress',
+}: LoaderProps) {
   return (
     <Box display="flex" justifyContent="center" p={p}>
-      <CircularProgress />
+      <CircularProgress aria-label={ariaLabel} />
     </Box>
   );
 }
